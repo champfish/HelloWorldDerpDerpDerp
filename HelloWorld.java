d5efb60d671f8fa6281f9ed1088cb9a01d7d895f
CJ Duffee <champfish@users.noreply.github.com>
2017-10-28 11:44:29 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,11 +3,19 @@ import java.System.out;
 public class HelloWorld {
   
   public static void main(String args[]) {
-    printHello();
-  }
-  
-  public static printHello() {
-    out.println("Hello World");
+    Printer hp = new Printer("Hello World");
   }
 
+
+}
+
+class Printer {
+  String toPrint;
+  public Printer(String toPrint){
+    this.toPrint = toPrint;
+  }
+  
+  public void print(){
+    out.print(toPrint+"\n");
+  }
 }
